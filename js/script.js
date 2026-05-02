9bc8c95bea3986f366f9b2d366523677bfdf7259
Adel <260263799+Adelvdw@users.noreply.github.com>
2026-05-02 14:17:31 +0200

    Attempt 1 at form Script (w10)
    Non-functional, needs to be redone TBC

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// // #Attempt 1, IDK if correct...?
+
+// let form = document.forms['contactForm'];
+// form.addEventListener("submit", getValues);
+
+// getValues = (event) => {
+//     event.preventDefault();
+
+//     let formData = {
+//        "name":this.name.value,
+//     }
+
+//     let out = `
+//         <p>Name: <span>${formData.name}</span></p>
+//         `
+
+//     document.querySelector(".code").innerHTML = out
+
+// }
+
+// console.log("Hello")

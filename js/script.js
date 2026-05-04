49486840fe9a2fa63bd0c10ffc409894bb6295e3
Adel <260263799+Adelvdw@users.noreply.github.com>
2026-05-04 13:50:45 +0200

    Adjusted Attempt 1 with some corrections

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 // // #Attempt 1, IDK if correct...?
 
-// let form = document.forms['contactForm'];
-// form.addEventListener("submit", getValues);
+//  let form = document.forms['contactForm'];
+//  form.addEventListener("submit", getValues);
 
-// getValues = (event) => {
-//     event.preventDefault();
 
-//     let formData = {
-//        "name":this.name.value,
-//     }
 
-//     let out = `
-//         <p>Name: <span>${formData.name}</span></p>
-//         `
 
-//     document.querySelector(".code").innerHTML = out
 
-// }
+/* getValues = (event) => {
+    event.preventDefault();
 
-// console.log("Hello")
+    let formData = {
+        "name":form.name.value,
+    }
+
+    let out = `
+        <p>Name: <span>${formData.name}</span></p>
+        `
+
+    document.querySelector(".code").innerHTML = out
+
+ }
+
+let form = document.forms['contactForm'];
+form.addEventListener("submit", getValues);
+
+ console.log("Hello") */
